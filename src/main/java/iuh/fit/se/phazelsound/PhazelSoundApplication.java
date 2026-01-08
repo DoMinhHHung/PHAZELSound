744956dea25abf0d025832ef6c12414d14d171a1
DoMinhHung <dominhhung04032003@gmail.com>
2026-01-08 20:35:09 +0700

    Init project

---
+++ src/main/java/iuh/fit/se/phazelsound/PhazelSoundApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.phazelsound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhazelSoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhazelSoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iuh/fit/se/phazelsound/PhazelSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.phazelsound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhazelSoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
